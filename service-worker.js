a3d45ec594cfcab5f99197635d2ff43d19a41624
AygunAVM <f.moonday@gmail.com>
2026-03-04 15:02:45 +0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,23 @@
-const CACHE_NAME = "aygun-v2-cache";
+const CACHE_NAME = "aygun-v4-cache";
 const assets = [
   "./",
   "./index.html",
-  "./style.css",
+  "./style.ccs",
   "./app.js",
   "./manifest.json",
-  "./data/urunler.json"
+  "./data/urunler.json",
+  "./data/rates.json"
 ];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+  self.skipWaiting();
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  if (e.request.url.includes('/data/')) {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
